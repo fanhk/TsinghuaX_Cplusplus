db1371443dc2c3141fbe323ca608a3ebfd7f7281
fanhangkai <fanoze@hotmail.com>
2015-03-17 19:53:20 +0800

    undo commit

--- c2/c2-1.cpp
+++ c2/c2-1.cpp
@@ -1,26 +1,26 @@
 #include <iostream>
 using namespace std;
-//int main()
-//{
-//	int n;
-//	cin >> n;
-//	int max, min;
-//	int sum = 0;
-//	int x;
-//	cin >> x;
-//	max = min = x;
-//	sum += x;
-//	while (n > 1) {
-//		cin >> x;
-//		sum += x;
-//		if (max < x) {
-//			max = x;
-//		}
-//		if (x < min) {
-//			min = x;
-//		}
-//		n--;
-//	}
-//	cout << sum << ' ' << min << ' ' << max << endl;
-//	return 0;
-//}
+int main()
+{
+	int n;
+	cin >> n;
+	int max, min;
+	int sum = 0;
+	int x;
+	cin >> x;
+	max = min = x;
+	sum += x;
+	while (n > 1) {
+		cin >> x;
+		sum += x;
+		if (max < x) {
+			max = x;
+		}
+		if (x < min) {
+			min = x;
+		}
+		n--;
+	}
+	cout << sum << ' ' << min << ' ' << max << endl;
+	return 0;
+}
